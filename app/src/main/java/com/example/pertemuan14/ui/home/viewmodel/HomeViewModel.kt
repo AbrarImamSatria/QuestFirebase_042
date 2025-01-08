993e1652ea990ff15f08efb70d94a856cf0de3a7
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-08 19:30:20 +0700

    Mengerjakan di kelas

---
+++ app/src/main/java/com/example/pertemuan14/ui/home/viewmodel/HomeViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.pertemuan14.ui.home.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan14.model.Mahasiswa
+import com.example.pertemuan14.repository.RepositoryMhs
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+
+class HomeViewModel(
+    private  val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+    var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs(){
+        viewModelScope.launch {
+            repositoryMhs.getAllMhs()
+                .onStart {
+                    mhsUIState = HomeUiState.Loading
+                }
+                .catch {
+                    mhsUIState = HomeUiState.Error(e = it)
+                }
+                .collect{
+                    mhsUIState = if (it.isEmpty()){
+                        HomeUiState.Error(Exception("Belum ada data mahasiswa"))
+                    }else{
+                        HomeUiState.Succes(it)
+                    }
+
+                }
+
+        }
+    }
+
+}
+
+sealed class HomeUiState {
+    object Loading:HomeUiState()
+
+    data class Succes(val data: List<Mahasiswa>) : HomeUiState()
+    data class Error(val e: Throwable) : HomeUiState()
+}
