4d8d1737c6b19e5c8b8ca0b9d15dbb8d2925881b
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-10 23:22:30 +0700

    membuat class InsertViewModel,fun updateState dan validateFields

--- app/src/main/java/com/example/pertemuan14/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/home/viewmodel/InsertViewModel.kt
@@ -1,6 +1,45 @@
 package com.example.pertemuan14.ui.home.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.pertemuan14.model.Mahasiswa
+import com.example.pertemuan14.repository.RepositoryMhs
+
+class InsertViewModel (
+    private val mhs: RepositoryMhs
+): ViewModel() {
+
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    // Memperbarui state berdasarkan input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
+
+    // validasi data input pengguna
+    fun validateFields(): Boolean{
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else "Nim tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenisKelamin =  if (event.jeniskelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else "Angkatan tidak boleh kosong",
+        )
+
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+}
 
 sealed class FormState {
     object Idle : FormState()
