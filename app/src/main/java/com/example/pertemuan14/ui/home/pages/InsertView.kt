3c62739021f5148468a49f6834d63cb42737a344
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-11 00:04:01 +0700

    membuat fun InsertMhsView

--- app/src/main/java/com/example/pertemuan14/ui/home/pages/InsertView.kt
+++ app/src/main/java/com/example/pertemuan14/ui/home/pages/InsertView.kt
@@ -12,21 +12,110 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan14.ui.PenyediaViewModel
 import com.example.pertemuan14.ui.home.viewmodel.FormErrorState
 import com.example.pertemuan14.ui.home.viewmodel.FormState
 import com.example.pertemuan14.ui.home.viewmodel.HomeUiState
 import com.example.pertemuan14.ui.home.viewmodel.InsertUiState
+import com.example.pertemuan14.ui.home.viewmodel.InsertViewModel
 import com.example.pertemuan14.ui.home.viewmodel.MahasiswaEvent
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.text.Normalizer.Form
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InsertMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val uiState = viewModel.uiState //State utama untuk loading, success, error
+    val uiEvent = viewModel.uiEvent //State untuk form dan validasi
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    //Observasi perubahan state untuk snackbar dan navigasi
+    LaunchedEffect (uiState) {
+        when (uiState) {
+            is FormState.Success -> {
+                println(
+                    "InsertMhsView: uiState is FormState.Success,navigate to home" + uiState.message
+                )
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message) //Tampilkan snackbar
+                }
+                delay(700)
+                //navigasi langsung
+                onNavigate()
+
+                viewModel.resetSnackBarMessage() // Reset snackbar state
+            }
+
+            is FormState.Error -> {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message)
+                }
+            }
+            else -> Unit
+        }
+    }
+
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                title = { Text("Tambah Mahasiswa") },
+                navigationIcon = {
+                    Button(onClick = onBack) {
+                        Text("Back")
+                    }
+                }
+            )
+        }
+    ){ padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            InsertBodyMhs(
+                uiState = uiEvent,
+                homeUiState = uiState,
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent)
+                },
+                onClick = {
+                    if (viewModel.validateFields()) {
+                        viewModel.insertMhs()
+                    }
+                }
+            )
+        }
+    }
+}
+
 
 @Composable
 fun InsertBodyMhs(
